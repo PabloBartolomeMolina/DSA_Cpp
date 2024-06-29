b9f4d464ddb2a062208aed784db0e4d2ee8c2dfb
Pablo Bartolomé <pablobart.me@gmail.com>
2024-06-29 12:43:56 +0200

    Create tests files for filters

---
+++ Test_Filters.h
@@ -0,0 +1,23 @@
+#pragma once
+/*
+ * File name : Test_Filters.h
+ * This files includes some tests for the different filter classes.
+ */
+
+/* ================================================================================================== */
+/* Includes */
+
+/* ================================================================================================== */
+/* Constants */
+#define TEST_LOWPASS_NOMINAL		0x00
+#define TEST_HIGHPASS_NOMINAL		0x01
+#define TEST_LOWPASS_ITERATION		0x02
+#define TEST_HIGHPASS_ITERATION		0x03
+#define TEST_LOWPASS_HIGHPASS		0x04
+#define TEST_HIGHPASS_LOWPASS		0x05
+
+/* ================================================================================================== */
+/* Variables */
+
+/* ================================================================================================== */
+/* Functions */
